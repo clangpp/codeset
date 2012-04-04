1c25934d3bb709dab1fb1e182119e1524bae8c91
yongtianzhang@gmail.com <yongtianzhang@gmail.com@f44d3efd-ca78-6c36-34b2-b961743994f4>
2012-04-04 02:19:34 +0000

    [disjoint_set] initialize directory architecture and functional codes, no unittest yet.
    git-svn-id: http://alex--code.googlecode.com/svn/trunk@51 f44d3efd-ca78-6c36-34b2-b961743994f4

---
+++ disjoint_set/src/test.cpp
@@ -0,0 +1,29 @@
+// test.cpp
+
+#include "disjoint_set/disjoint_set.h"
+
+#include <iostream>
+
+#include "logging/logging.h"
+
+using namespace std;
+using namespace logging;
+
+void test_union_sets();
+void test_find();
+void test_disjoint_set();
+
+int main() {
+    test_union_sets();
+    test_find();
+    system("pause");
+}
+
+void test_union_sets() {
+}
+
+void test_find() {
+}
+
+void test_disjoint_set() {
+}
