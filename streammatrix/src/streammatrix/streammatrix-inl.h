2f64b3b177b582b9866b44a7c5b5d45b67d78455
yongtianzhang@gmail.com <yongtianzhang@gmail.com@f44d3efd-ca78-6c36-34b2-b961743994f4>
2012-04-25 11:18:05 +0000

    [streammatrix] 1. correct STREAMMATRIX_INL_H_ type error; 2. add class StreamMatrix.
    git-svn-id: http://alex--code.googlecode.com/svn/trunk@84 f44d3efd-ca78-6c36-34b2-b961743994f4

--- streammatrix/src/streammatrix/streammatrix-inl.h
+++ streammatrix/src/streammatrix/streammatrix-inl.h
@@ -1,9 +1,9 @@
 // streammatrix-inl.h
-#ifndef STREAMMATRIX_H_
-#define STREAMMATRIX_H_
+#ifndef STREAMMATRIX_INL_H_
+#define STREAMMATRIX_INL_H_
 
 #include "streammatrix.h"
 
 
 
-#endif  // STREAMMATRIX_H_
+#endif  // STREAMMATRIX_INL_H_
--- streammatrix/src/streammatrix/streammatrix.h
+++ streammatrix/src/streammatrix/streammatrix.h
@@ -2,10 +2,23 @@
 #ifndef STREAMMATRIX_H_
 #define STREAMMATRIX_H_
 
+#include <iostream>
+
 #include "../crosslist/crosslist.h"
 #include "../serialization/serialization.h"
 
+template <typename T>
+class StreamMatrix {
+public:
+	typedef T value_type;
+	typedef crosslist::size_type size_type;
 
+public:
+	size_type row_size(size_type row_index);
+	size_type column_size(size_type col_index);
+	size_type row_count() const;
+	size_type column_count() const;
+};
 
 #include "streammatrix-inl.h"
 
