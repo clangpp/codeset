ddcc15030028c8febe164db1e680c9ba5eb95425
yongtianzhang@gmail.com <yongtianzhang@gmail.com@f44d3efd-ca78-6c36-34b2-b961743994f4>
2012-09-16 08:41:44 +0000

    rename list to listing.
    git-svn-id: http://alex--code.googlecode.com/svn/trunk@107 f44d3efd-ca78-6c36-34b2-b961743994f4

---
+++ listing/src/listing/listing.h
@@ -0,0 +1,24 @@
+﻿// listing.h
+#ifndef LISTING_H_
+#define LISTING_H_
+
+namespace listing {
+
+/***************************************
+concept NodeT {
+    T value;
+    NodeT * next;
+};
+***************************************/
+
+template <typename NodeT>
+NodeT* reverse(NodeT* head);
+
+template <typename NodeT>
+NodeT* reverse_recursive(NodeT* head);
+
+}  // namespace listing
+
+#include "listing-inl.h"
+
+#endif  // LISTING_H_
