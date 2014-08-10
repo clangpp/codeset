a47df14a01c1035b62749a69b42e1344d707e4d1
clangpp <clangpp@gmail.com>
2014-08-11 01:15:37 +0800

    Adds a simple matrix class to show C++ features.

---
+++ matrix/src/test.cpp
@@ -0,0 +1,50 @@
+#include "matrix/matrix.h"
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+  LOG << "`Matrix<double> my(3,3)`";
+  Matrix<double> my(3,3);
+  my(2,2) = 2.12;
+  cout << endl << endl;
+
+  LOG << "`cout << my`";
+  cout << my;
+  cout << endl << endl;
+
+  LOG << "`Matrix <double> aaa(my)`";
+  Matrix<double> aaa(my);
+  cout << endl << endl;
+
+  LOG << "Constructing `bbb`";
+  Matrix<double> bbb(my);
+  cout << endl << endl;
+
+  LOG << "`bbb = aaa + my + bbb`";
+  bbb = aaa + my + bbb;
+  cout << endl << endl;
+
+  LOG << "`bbb = aaa + (my + bbb)`";
+  bbb = aaa + (my + bbb);
+  cout << endl << endl;
+
+  LOG << "`Matrix<double> ccc = aaa + my + bbb`";
+  Matrix<double> ccc = aaa + my + bbb;
+  cout << endl << endl;
+
+  LOG << "`Matrix<double> ddd=aaa`";
+  Matrix<double> ddd=aaa;
+  cout << endl << endl;
+
+  LOG << "`ddd = bbb`";
+  ddd = bbb;
+  cout << endl << endl;
+
+  LOG << "`Matrix<double> eee(aaa + bbb)`";
+  Matrix<double> eee(aaa + bbb);
+  cout << endl << endl;
+
+  return 0;
+}
