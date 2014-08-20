efb1d299efbb3a1eb408c522bd3d01fa1b0b59fd
clangpp <clangpp@gmail.com>
2014-08-20 22:23:21 +0800

    Adds graph-based solution: Expresses every node (variable) in a connected subgraph by its first node (variable), solve the very first node (variable) if possible.

---
+++ problems/google_apac_test_for_2015/c_addition/graph_solution.cc
@@ -0,0 +1,217 @@
+#include <algorithm>
+#include <cstddef>  // size_t
+#include <iostream>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <vector>
+
+using namespace std;
+
+typedef size_t Variable;
+
+struct Edge {
+  Variable to;
+  int sum;
+  Edge(Variable to, int sum): to(to), sum(sum) {}
+  Edge(): to(0), sum(0) {}
+};
+
+class NameIndexDict {
+ public:
+  size_t IndexOf(const string& name) {
+    if (!name_to_index_.count(name)) {
+      name_to_index_.insert(make_pair(name, index_to_name_.size()));
+      index_to_name_.push_back(name);
+    }
+    return name_to_index_[name];
+  }
+  bool HasName(const string& name) { return name_to_index_.count(name) > 0; }
+  const string& NameOf(size_t index) {
+    return index_to_name_.at(index);  // uses exception to check input range
+  }
+  size_t size() const { return index_to_name_.size(); }
+ private:
+  unordered_map<string, size_t> name_to_index_;
+  vector<string> index_to_name_;
+};
+
+struct Expression {
+  int constant;
+  vector<Variable> variables;
+  vector<int> coefficients;
+
+  Expression(int constant, Variable variable, int coefficient)
+      : constant(constant), variables(1, variable),
+        coefficients(1, coefficient) {
+  }
+  explicit Expression(int constant): constant(constant) {}
+  Expression(): constant(0) {}
+
+  bool operator != (const Expression& other) const {
+    return !(constant == other.constant &&
+             variables == other.variables &&
+             coefficients == other.coefficients);
+  }
+
+  Expression operator - () const {
+    Expression result(*this);
+    result.constant = -result.constant;
+    for (auto& coefficient : result.coefficients) {
+      coefficient = -coefficient;
+    }
+    return result;
+  }
+
+  Expression operator + (const Expression& other) const {
+    Expression result;
+    result.constant = constant + other.constant;
+    for (size_t i = 0, j = 0;
+         i < variables.size() || j < other.variables.size(); ) {
+      if (i >= variables.size()) {
+        for (; j < other.variables.size(); ++j) {
+          result.variables.push_back(other.variables[j]);
+          result.coefficients.push_back(other.coefficients[j]);
+        }
+      } else if (j >= other.variables.size()) {
+        for (; i < variables.size(); ++i) {
+          result.variables.push_back(variables[i]);
+          result.coefficients.push_back(coefficients[i]);
+        }
+      } else {
+        if (variables[i] < other.variables[j]) {
+          result.variables.push_back(variables[i]);
+          result.coefficients.push_back(coefficients[i]);
+          ++i;
+        } else if (variables[i] > other.variables[j]) {
+          result.variables.push_back(other.variables[j]);
+          result.coefficients.push_back(other.coefficients[j]);
+          ++j;
+        } else {  // variables[i] == other.variables[j]
+          int coefficient = coefficients[i] + other.coefficients[j];
+          if (coefficient != 0) {
+            result.variables.push_back(variables[i]);
+            result.coefficients.push_back(coefficient);
+          }
+          ++i;
+          ++j;
+        }
+      }
+    }
+    return result;
+  }
+
+  Expression operator - (const Expression& other) const {
+    return *this + (-other);
+  }
+
+  // friend ostream& operator << (ostream& os, const Expression& exp) {
+  //   os << exp.constant;
+  //   for (size_t i = 0; i < exp.variables.size(); ++i) {
+  //     if (exp.coefficients[i] > 0) {
+  //       os << "+";
+  //     }
+  //     os << exp.coefficients[i] << "*x_" << exp.variables[i];
+  //   }
+  //   return os;
+  // }
+};
+
+int main(int argc, char* argv[]) {
+  int T = 0;
+  cin >> T;
+  for (int t = 0; t < T; ++t) {
+    // Processes evidences, builds graph.
+    int N = 0;
+    cin >> N;
+    vector<vector<Edge>> edges;
+    NameIndexDict dict;
+    for (int n = 0; n < N; ++n) {
+      // Parses input edge
+      string unused, var1, var2;
+      getline(cin, unused);  // new line
+      getline(cin, var1, '+');
+      getline(cin, var2, '=');
+      int sum = 0;
+      cin >> sum;
+      Edge edge;
+      size_t index1 = dict.IndexOf(var1);
+      size_t index2 = dict.IndexOf(var2);
+
+      // Puts edge into graph
+      edges.resize(dict.size());
+      if (index1 == index2) {  // not edge
+        edges[index1].push_back(Edge(index2, sum));
+      } else {
+        edges[index1].push_back(Edge(index2, sum));
+        edges[index2].push_back(Edge(index1, sum));
+      }
+    }
+
+    // Preprocesses graph, calulates variable representation.
+    size_t num_vars = edges.size();
+    vector<Expression> exps(num_vars);
+    vector<bool> determined(num_vars);
+    vector<int> values(num_vars);
+    // DFS, Expresses every node in connected subgraph by first visited node.
+    vector<bool> visited(num_vars);
+    stack<Variable> holder;
+    for (int var = (int)num_vars-1; var >= 0; --var) {
+      holder.push(var);
+    }
+    while (!holder.empty()) {
+      Variable var = holder.top();
+      holder.pop();
+      if (!visited[var]) {
+        exps[var] = Expression(0, var, 1);  // a = a
+        visited[var] = true;
+      }
+      for (const auto& edge : edges[var]) {
+        Expression exp = Expression(edge.sum) - exps[var];  // b = n-a
+        if (visited[edge.to] && exps[edge.to] != exp) {
+          // Calculates first visited node value (constant).
+          determined[exp.variables.front()] = true;
+          values[exp.variables.front()] =
+              (exp.constant - exps[edge.to].constant) /
+              (exps[edge.to].coefficients.front() - exp.coefficients.front());
+        }
+        if (!visited[edge.to]) {
+          exps[edge.to] = exp;
+          visited[edge.to] = true;
+          holder.push(edge.to);
+        }
+      }
+    }
+
+    // Handles requests
+    cout << "Case #" << (t+1) << ":" << endl;
+    int Q = 0;
+    cin >> Q;
+    for (int q = 0; q < Q; ++q) {
+      string unused, var1, var2;
+      getline(cin, unused);  // new line
+      getline(cin, var1, '+');
+      cin >> var2;
+      if (!dict.HasName(var1) || !dict.HasName(var2)) {
+        continue;  // new variable, can't determine
+      }
+      size_t index1 = dict.IndexOf(var1);
+      size_t index2 = dict.IndexOf(var2);
+      Expression sum_exp = exps[index1] + exps[index2];
+      int sum = sum_exp.constant;
+      bool sum_determined = true;
+      for (size_t i = 0; i < sum_exp.variables.size(); ++i) {
+        if (determined[sum_exp.variables[i]]) {
+          sum += values[sum_exp.variables[i]] * sum_exp.coefficients[i];
+        } else {
+          sum_determined = false;
+          break;
+        }
+      }
+      if (sum_determined) {
+        cout << var1 << "+" << var2 << "=" << sum << endl;
+      }
+    }
+  }
+  return 0;
+}
--- problems/google_apac_test_for_2015/c_addition/run.sh
+++ problems/google_apac_test_for_2015/c_addition/run.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-g++ -std=c++0x solution.cc && ./a.out < input.txt
+g++ -std=c++0x graph_solution.cc && ./a.out < input.txt
+# g++ -std=c++0x graph_solution.cc && ./a.out < ~/Downloads/C-small-practice.in > small-practice.out
+# g++ -std=c++0x graph_solution.cc && ./a.out < ~/Downloads/C-large-practice.in > large-practice.out
