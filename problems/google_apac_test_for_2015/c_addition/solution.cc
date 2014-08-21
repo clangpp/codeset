66b9c4f008ef132bbab428c9bf51876768222176
clangpp <clangpp@gmail.com>
2014-08-21 13:53:40 +0800

    Fixes logic error: missed case a+a=3, b+b=5 => a+b

--- problems/google_apac_test_for_2015/c_addition/solution.cc
+++ problems/google_apac_test_for_2015/c_addition/solution.cc
@@ -1,13 +1,26 @@
 // STATUS: small input passed, large input passed.
 //
-// my idea:
-//  1. Evidence x+y=n is an edge of a graph, with x and y as nodes, and n as weight.
-//  2. If x to y has odd number edges (n1, n2, n3, ..., nt), then x+y is determined as n1-n2+n3-...+nt.
-//  3. If there's evidence z+z=n, then z is determined (n/2), and thus all nodes connected with z are determined.
-//    But no need for determination, one path of (n1, n2, ..., nz, ..., nt) and (n1, n2, ..., nz, n(z-to-z), ... nt) must has odd number edges, then x+y can be determined by rule 2.
-//  4. If there's circle (z to z) which has odd number edges (n1, n2, n3, ..., nt), then z is determined as (n1-n2+n3-...+nt)/2, thus all nodes connected with z are determined.
-//    But no need for determination, one path of (n1, n2, ..., nz, ..., nt) and (n1, n2, ..., nz, n(z-to-circle1), ..., n(circle-1-to-z), ..., nt) must has odd number edges, then x+y can be determined by rule 2.
-//  5. Otherwise, x+y can't be determined.
+// Idea:
+//  1. Edge x+y=n is an edge of a graph, with x and y as nodes, and n as
+//    weight.
+//  2. If x to y has odd number edges (n1, n2, n3, ..., nt), then x+y is
+//    determined as n1-n2+n3-...+nt.
+//  3. If there's edge z+z=n, then z is determined (n/2), and thus all nodes
+//    connected with z are determined.
+//    But normally no need for determination, one path of
+//    (n1, n2, ..., nz, ..., nt) and
+//    (n1, n2, ..., nz, n(z-to-z), ... nt) must has odd number edges, then x+y
+//    can be determined by rule 2.
+//  4. If there's circle (z to z) which has odd number edges
+//    (n1, n2, n3, ..., nt), then z is determined as (n1-n2+n3-...+nt)/2, thus
+//    all nodes connected with z are determined.
+//    But normally no need for determination, one path of
+//    (n1, n2, ..., nz, ..., nt) and
+//    (n1, n2, ..., nz, n(z-to-circle1), ..., n(circle-1-to-z), ..., nt) must
+//    has odd number edges, then x+y can be determined by rule 2.
+//  5. Unnormally, if x and y are not connected, but x and y can both be
+//    determined by rule 3 or rule 4, then x+y can still be determined.
+//  6. Otherwise, x+y can't be determined.
 
 #include <cstddef>  // for size_t
 #include <iostream>
@@ -79,6 +92,37 @@ bool DetermineSum(
   return false;
 }
 
+bool DetermineValue(
+    const vector<vector<Edge>>& edges, size_t var0, int* value_times_2) {
+  vector<bool> visited(edges.size());
+  vector<int> var0_plus(edges.size());
+  queue<size_t> determined_nodes;
+  for (const auto& edge : edges[var0]) {
+    var0_plus[edge.to] = edge.sum;
+    visited[edge.to] = true;
+    determined_nodes.push(edge.to);
+  }
+  for (; !determined_nodes.empty(); determined_nodes.pop()) {
+    size_t start = determined_nodes.front();
+    if (start == var0) {
+      *value_times_2 = var0_plus[var0];
+      return true;
+    }
+    for (const auto& edge1 : edges[start]) {
+      size_t via = edge1.to;
+      for (const auto& edge2 : edges[via]) {
+        size_t next = edge2.to;
+        if (!visited[next]) {
+          var0_plus[next] = var0_plus[start] - edge1.sum + edge2.sum;
+          visited[next] = true;
+          determined_nodes.push(next);
+        }
+      }
+    }
+  }
+  return false;
+}
+
 int main(int argc, char* argv[]) {
   int T = 0;
   cin >> T;
@@ -123,9 +167,13 @@ int main(int argc, char* argv[]) {
       }
       size_t index1 = dict.IndexOf(var1);
       size_t index2 = dict.IndexOf(var2);
-      int sum = 0;
+      int sum = 0, var1_times_2 = 0, var2_times_2 = 0;
       if (DetermineSum(edges, index1, index2, &sum)) {
         cout << var1 << "+" << var2 << "=" << sum << endl;
+      } else if (DetermineValue(edges, index1, &var1_times_2) &&
+                 DetermineValue(edges, index2, &var2_times_2)) {
+        cout << var1 << "+" << var2 << "="
+             << (var1_times_2 + var2_times_2) / 2  << endl;
       }
     }
   }
