517ebf68baf23e0ce55171e5b6b1da44d7668571
clangpp <clangpp@gmail.com>
2014-08-22 18:13:49 +0800

    Adds corner case to test data.

--- problems/google_apac_test_for_2015/c_addition/run.sh
+++ problems/google_apac_test_for_2015/c_addition/run.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-g++ -std=c++0x graph_solution.cc && ./a.out < input.txt
-# g++ -std=c++0x graph_solution.cc && ./a.out < ~/Downloads/C-small-practice.in > small-practice.out
-# g++ -std=c++0x graph_solution.cc && ./a.out < ~/Downloads/C-large-practice.in > large-practice.out
+clang++ -std=c++0x graph_solution.cc && ./a.out < input.txt
+clang++ -std=c++0x solution.cc && ./a.out < input.txt
