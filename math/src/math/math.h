5cce968aa4be72204ec68214a37cf8fd9e5002fb
clangpp <clangpp@gmail.com>
2014-08-23 05:53:26 +0800

    Adds Gauss-Jordan Elimination concurrent implementation.

--- math/src/math/math.h
+++ math/src/math/math.h
@@ -1,9 +1,73 @@
 #ifndef MATH_H_
 #define MATH_H_
 
+#include <algorithm>
+#include <cmath>
+#include <future>
+#include <sstream>
+#include <utility>
+#include <vector>
+
+#include "../../../matrix/src/matrix/matrix.h"
+
 namespace math {
 
+void CheckLess(size_t lhs, size_t rhs) {
+  if (!(lhs < rhs)) {
+    std::stringstream ss;
+    ss << "`lhs < rhs` expected, actual " << lhs << " and " << rhs;
+    throw std::runtime_error(ss.str());
+  }
+}
 
+template <typename T>
+struct AbsoluteValue {
+  T operator()(const T& value) const { return std::abs(value); }
+};
+
+// abs: `abs(value1) < abs(value2)` should be satified.
+template <typename T, typename UnaryFunction = AbsoluteValue<T>>
+void GaussJordanEliminate(Matrix<T>* mat,
+                          UnaryFunction abs = UnaryFunction()) {
+  // Augmented matrix has at least one column.
+  CheckLess(0, mat->column_size());
+  typedef typename Matrix<T>::size_type size_type;
+  std::vector<std::future<void>> futures(mat->row_size());
+
+  // Augmented matrix represents exactly max->column_size()-1 variables.
+  size_type dimension = std::min(mat->row_size(), mat->column_size()-1);
+  for (size_type sentry = 0; sentry < dimension; ++sentry) {
+    // Finds (abs) max element of current column.
+    auto max_iter = std::max_element(
+        mat->column_begin(sentry) + sentry, mat->column_end(sentry),
+        [&abs](const T& lhs, const T& rhs) {
+          return abs(lhs) < abs(rhs);
+        });
+    size_type max_row = max_iter - mat->column_begin(sentry);
+    if ((*mat)[max_row][sentry] == 0) {
+      continue;
+    }
+
+    // Concurrently eliminates all rows (except `max_row`) in column `sentry`.
+    for (size_type row = 0; row < mat->row_size(); ++row) {
+      futures[row] = std::async(
+          std::launch::async,
+          [mat, sentry, max_row, row]() {
+            if (row != max_row) {
+              mat->elementary_row_add(
+                  row, max_row, -(*mat)[row][sentry]/(*mat)[max_row][sentry]);
+            }
+          });
+    }
+    for (size_type row = 0; row < mat->row_size(); ++row) {
+      futures[row].wait();
+    }
+
+    // Normalizes `max_row` and switches to `sentry` row.
+    mat->elementary_row_multiply(max_row, 1 / (*mat)[max_row][sentry]);
+    mat->elementary_row_switch(sentry, max_row);
+  }
+}
 
 }  // namespace math
 
--- math/src/math/math_test.cc
+++ math/src/math/math_test.cc
@@ -1,7 +1,89 @@
 #include "math.h"
 
 #include <cassert>
+#include <cmath>
+#include <iostream>
+
+using namespace std;
+
+bool EQ(double lhs, double rhs) {
+  return abs(lhs - rhs) < 1e-6;
+}
+
+void TestGaussJordanEliminate() {
+  // 8, -2
+  Matrix<double> a(2, 3);
+  a[0][0] = 3; a[0][1] = 9; a[0][2] = 6;
+  a[1][0] = 2; a[1][1] = 4; a[1][2] = 8;
+
+  math::GaussJordanEliminate(&a);
+
+  assert(EQ(a[0][0], 1)); assert(EQ(a[0][1], 0)); assert(EQ(a[0][2], 8));
+  assert(EQ(a[1][0], 0)); assert(EQ(a[1][1], 1)); assert(EQ(a[1][2], -2));
+
+  // 2, 3, 4
+  Matrix<double> b(5, 4);
+  b[0][0] = 5; b[0][1] = 2; b[0][2] = 1; b[0][3] = 20;
+  b[1][0] = 4; b[1][1] = 3; b[1][2] = 2; b[1][3] = 25;
+  b[2][0] = 2; b[2][1] = 4; b[2][2] = 3; b[2][3] = 28;
+  b[3][0] = 3; b[3][1] = 5; b[3][2] = 4; b[3][3] = 37;
+  b[4][0] = 1; b[4][1] = 2; b[4][2] = 3; b[4][3] = 20;
+
+  math::GaussJordanEliminate(&b);
+
+  assert(EQ(b[0][0], 1)); assert(EQ(b[0][1], 0));
+  assert(EQ(b[0][2], 0)); assert(EQ(b[0][3], 2));
+
+  assert(EQ(b[1][0], 0)); assert(EQ(b[1][1], 1));
+  assert(EQ(b[1][2], 0)); assert(EQ(b[1][3], 3));
+
+  assert(EQ(b[2][0], 0)); assert(EQ(b[2][1], 0));
+  assert(EQ(b[2][2], 1)); assert(EQ(b[2][3], 4));
+
+  for (int j = 0; j < 4; ++j) {
+    assert(EQ(b[3][j], 0));
+    assert(EQ(b[4][j], 0));
+  }
+
+  // 2, 0, 4
+  Matrix<double> c(5, 4);
+  c[0][0] = 5; c[0][1] = 2; c[0][2] = 1; c[0][3] = 14;
+  c[1][0] = 4; c[1][1] = 3; c[1][2] = 2; c[1][3] = 16;
+  c[2][0] = 2; c[2][1] = 4; c[2][2] = 3; c[2][3] = 16;
+  c[3][0] = 3; c[3][1] = 5; c[3][2] = 4; c[3][3] = 22;
+  c[4][0] = 1; c[4][1] = 2; c[4][2] = 3; c[4][3] = 14;
+
+  math::GaussJordanEliminate(&c);
+
+  assert(EQ(c[0][0], 1)); assert(EQ(c[0][1], 0));
+  assert(EQ(c[0][2], 0)); assert(EQ(c[0][3], 2));
+
+  assert(EQ(c[1][0], 0)); assert(EQ(c[1][1], 1));
+  assert(EQ(c[1][2], 0)); assert(EQ(c[1][3], 0));
+
+  assert(EQ(c[2][0], 0)); assert(EQ(c[2][1], 0));
+  assert(EQ(c[2][2], 1)); assert(EQ(c[2][3], 4));
+
+  for (int j = 0; j < 4; ++j) {
+    assert(EQ(c[3][j], 0));
+    assert(EQ(c[4][j], 0));
+  }
+
+  // 2, 3, (4)
+  Matrix<double> d(2, 4);
+  d[0][0] = 2; d[0][1] = 3; d[0][2] = 4; d[0][3] = 29;
+  d[1][0] = 4; d[1][1] = 3; d[1][2] = 2; d[1][3] = 25;
+
+  math::GaussJordanEliminate(&d);
+
+  assert(EQ(d[0][0], 1)); assert(EQ(d[0][1], 0));
+  assert(EQ(d[0][2], -1)); assert(EQ(d[0][3], -2));
+
+  assert(EQ(d[1][0], 0)); assert(EQ(d[1][1], 1));
+  assert(EQ(d[1][2], 2)); assert(EQ(d[1][3], 11));
+}
 
 int main(int argc, char* argv[]) {
+  TestGaussJordanEliminate();
   return 0;
 }
