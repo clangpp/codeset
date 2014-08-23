6ff0adcd9f67b4baa14a9d07e2fb189049c4321d
clangpp <clangpp@gmail.com>
2014-08-24 01:48:40 +0800

    Redefines GaussJordanEliminate() interface, puts constants in another matrix.

--- math/src/math/math.h
+++ math/src/math/math.h
@@ -13,11 +13,10 @@
 
 namespace math {
 
-void CheckSizeAtLeast(size_t test_size, size_t least_size) {
-  if (!(test_size >= least_size)) {
+void CheckSizeEqual(size_t size1, size_t size2) {
+  if (!(size1 == size2)) {
     std::stringstream ss;
-    ss << "`test_size >= least_size` expected, actual "
-        << test_size << " and " << least_size;
+    ss << "`size1 == size2` expected, actual " << size1 << " and " << size2;
     throw std::runtime_error(ss.str());
   }
 }
@@ -26,51 +25,62 @@ void CheckSizeAtLeast(size_t test_size, size_t least_size) {
 // special rules for value type, e.g. is_zero(double v) -> std::abs(v) < 1e-6;
 template <typename T>
 void GaussJordanEliminate(
-    Matrix<T>* augmented_matrix,
+    Matrix<T>* coefficient_matrix,
+    Matrix<T>* constant_matrix = nullptr,
     std::function<bool(const T&, const T&)> absolute_less =
         [](const T& lhs, const T& rhs) {
           return std::abs(lhs) < std::abs(rhs);
         },
     std::function<bool(const T&)> is_zero =
         [](const T& value) { return value == 0; }) {
-  // Augmented matrix has at least one column.
-  CheckSizeAtLeast(augmented_matrix->column_size(), 1);
+  if (constant_matrix) {
+    CheckSizeEqual(coefficient_matrix->row_size(),
+                   constant_matrix->row_size());
+  }
 
   // Helpers
-  auto mat = augmented_matrix;  // shorter name
+  auto a_mat = coefficient_matrix;  // shorter name
+  auto b_mat = constant_matrix;  // shorter name
   typedef typename Matrix<T>::size_type size_type;
-  std::vector<std::future<void>> futures(mat->row_size());
+  std::vector<std::future<void>> futures(a_mat->row_size());
 
-  // Augmented matrix represents exactly max->column_size()-1 variables.
-  size_type dimension = std::min(mat->row_size(), mat->column_size()-1);
+  size_type dimension = std::min(a_mat->row_size(), a_mat->column_size());
   for (size_type pivot = 0; pivot < dimension; ++pivot) {
-    // Finds (abs) max element of current column.
+    // Finds max (absolute) value of current column.
     auto max_iter = std::max_element(
-        mat->column_begin(pivot) + pivot, mat->column_end(pivot),
+        a_mat->column_begin(pivot) + pivot, a_mat->column_end(pivot),
         absolute_less);
-    size_type max_row = max_iter - mat->column_begin(pivot);
-    if (is_zero((*mat)[max_row][pivot])) {
+    size_type max_row = max_iter - a_mat->column_begin(pivot);
+    if (is_zero((*a_mat)[max_row][pivot])) {
       continue;
     }
 
     // Concurrently eliminates all rows (except `max_row`) in column `pivot`.
-    for (size_type row = 0; row < mat->row_size(); ++row) {
+    for (size_type row = 0; row < a_mat->row_size(); ++row) {
       futures[row] = std::async(
           std::launch::async,
-          [mat, pivot, max_row, row]() {
+          [a_mat, b_mat, pivot, max_row, row]() {
             if (row != max_row) {
-              mat->elementary_row_add(
-                  row, max_row, -(*mat)[row][pivot]/(*mat)[max_row][pivot]);
+              T factor = -(*a_mat)[row][pivot] / (*a_mat)[max_row][pivot];
+              a_mat->elementary_row_add(row, max_row, factor);
+              if (b_mat) {
+                b_mat->elementary_row_add(row, max_row, factor);
+              }
             }
           });
     }
-    for (size_type row = 0; row < mat->row_size(); ++row) {
+    for (size_type row = 0; row < a_mat->row_size(); ++row) {
       futures[row].wait();
     }
 
     // Normalizes `max_row` and switches to `pivot` row.
-    mat->elementary_row_multiply(max_row, 1 / (*mat)[max_row][pivot], is_zero);
-    mat->elementary_row_switch(pivot, max_row);
+    T factor = 1 / (*a_mat)[max_row][pivot];
+    a_mat->elementary_row_multiply(max_row, factor, is_zero);
+    a_mat->elementary_row_switch(pivot, max_row);
+    if (b_mat) {
+      b_mat->elementary_row_multiply(max_row, factor, is_zero);
+      b_mat->elementary_row_switch(pivot, max_row);
+    }
   }
 }
 
--- math/src/math/math_test.cc
+++ math/src/math/math_test.cc
@@ -6,85 +6,110 @@
 
 using namespace std;
 
-bool EQ(double lhs, double rhs) {
+bool EQ(const double& lhs, const double& rhs) {
   return abs(lhs - rhs) < 1e-6;
 }
 
 void TestGaussJordanEliminate() {
   // 8, -2
-  Matrix<double> a(2, 3);
-  a[0][0] = 3; a[0][1] = 9; a[0][2] = 6;
-  a[1][0] = 2; a[1][1] = 4; a[1][2] = 8;
+  Matrix<double> a = {
+    {3, 9},
+    {2, 4},
+  }, aa = {
+    {6},
+    {8},
+  };
 
-  math::GaussJordanEliminate(&a);
+  math::GaussJordanEliminate(&a, &aa);
 
-  assert(EQ(a[0][0], 1)); assert(EQ(a[0][1], 0)); assert(EQ(a[0][2], 8));
-  assert(EQ(a[1][0], 0)); assert(EQ(a[1][1], 1)); assert(EQ(a[1][2], -2));
+  assert(EQ(a[0][0], 1)); assert(EQ(a[0][1], 0));
+  assert(EQ(a[1][0], 0)); assert(EQ(a[1][1], 1));
+
+  assert(EQ(aa[0][0], 8));
+  assert(EQ(aa[1][0], -2));
 
   // 2, 3, 4
-  Matrix<double> b(5, 4);
-  b[0][0] = 5; b[0][1] = 2; b[0][2] = 1; b[0][3] = 20;
-  b[1][0] = 4; b[1][1] = 3; b[1][2] = 2; b[1][3] = 25;
-  b[2][0] = 2; b[2][1] = 4; b[2][2] = 3; b[2][3] = 28;
-  b[3][0] = 3; b[3][1] = 5; b[3][2] = 4; b[3][3] = 37;
-  b[4][0] = 1; b[4][1] = 2; b[4][2] = 3; b[4][3] = 20;
+  Matrix<double> b = {
+    {5, 2, 1},
+    {4, 3, 2},
+    {2, 4, 3},
+    {3, 5, 4},
+    {1, 2, 3},
+  }, bb = {
+    {20},
+    {25},
+    {28},
+    {37},
+    {20},
+  };
 
-  math::GaussJordanEliminate(&b);
+  math::GaussJordanEliminate(&b, &bb);
 
-  assert(EQ(b[0][0], 1)); assert(EQ(b[0][1], 0));
-  assert(EQ(b[0][2], 0)); assert(EQ(b[0][3], 2));
-
-  assert(EQ(b[1][0], 0)); assert(EQ(b[1][1], 1));
-  assert(EQ(b[1][2], 0)); assert(EQ(b[1][3], 3));
-
-  assert(EQ(b[2][0], 0)); assert(EQ(b[2][1], 0));
-  assert(EQ(b[2][2], 1)); assert(EQ(b[2][3], 4));
-
-  for (int j = 0; j < 4; ++j) {
+  assert(EQ(b[0][0], 1)); assert(EQ(b[0][1], 0)); assert(EQ(b[0][2], 0));
+  assert(EQ(b[1][0], 0)); assert(EQ(b[1][1], 1)); assert(EQ(b[1][2], 0));
+  assert(EQ(b[2][0], 0)); assert(EQ(b[2][1], 0)); assert(EQ(b[2][2], 1));
+  for (int j = 0; j < 3; ++j) {
     assert(EQ(b[3][j], 0));
     assert(EQ(b[4][j], 0));
   }
 
+  assert(EQ(bb[0][0], 2));
+  assert(EQ(bb[1][0], 3));
+  assert(EQ(bb[2][0], 4));
+  assert(EQ(bb[3][0], 0));
+  assert(EQ(bb[4][0], 0));
+
   // 2, 0, 4
-  Matrix<double> c(5, 4);
-  c[0][0] = 5; c[0][1] = 2; c[0][2] = 1; c[0][3] = 14;
-  c[1][0] = 4; c[1][1] = 3; c[1][2] = 2; c[1][3] = 16;
-  c[2][0] = 2; c[2][1] = 4; c[2][2] = 3; c[2][3] = 16;
-  c[3][0] = 3; c[3][1] = 5; c[3][2] = 4; c[3][3] = 22;
-  c[4][0] = 1; c[4][1] = 2; c[4][2] = 3; c[4][3] = 14;
+  Matrix<double> c = {
+    {5, 2, 1},
+    {4, 3, 2},
+    {2, 4, 3},
+    {3, 5, 4},
+    {1, 2, 3},
+  }, cc = {
+    {14},
+    {16},
+    {16},
+    {22},
+    {14},
+  };
 
-  math::GaussJordanEliminate(&c);
+  math::GaussJordanEliminate(&c, &cc);
 
-  assert(EQ(c[0][0], 1)); assert(EQ(c[0][1], 0));
-  assert(EQ(c[0][2], 0)); assert(EQ(c[0][3], 2));
-
-  assert(EQ(c[1][0], 0)); assert(EQ(c[1][1], 1));
-  assert(EQ(c[1][2], 0)); assert(EQ(c[1][3], 0));
-
-  assert(EQ(c[2][0], 0)); assert(EQ(c[2][1], 0));
-  assert(EQ(c[2][2], 1)); assert(EQ(c[2][3], 4));
-
-  for (int j = 0; j < 4; ++j) {
+  assert(EQ(c[0][0], 1)); assert(EQ(c[0][1], 0)); assert(EQ(c[0][2], 0));
+  assert(EQ(c[1][0], 0)); assert(EQ(c[1][1], 1)); assert(EQ(c[1][2], 0));
+  assert(EQ(c[2][0], 0)); assert(EQ(c[2][1], 0)); assert(EQ(c[2][2], 1));
+  for (int j = 0; j < 3; ++j) {
     assert(EQ(c[3][j], 0));
     assert(EQ(c[4][j], 0));
   }
 
+  assert(EQ(cc[0][0], 2));
+  assert(EQ(cc[1][0], 0));
+  assert(EQ(cc[2][0], 4));
+  assert(EQ(cc[3][0], 0));
+  assert(EQ(cc[4][0], 0));
+
+
   // 2, 3, (4)
-  Matrix<double> d(2, 4);
-  d[0][0] = 2; d[0][1] = 3; d[0][2] = 4; d[0][3] = 29;
-  d[1][0] = 4; d[1][1] = 3; d[1][2] = 2; d[1][3] = 25;
+  Matrix<double> d = {
+    {2, 3, 4},
+    {4, 3, 2},
+  }, dd = {
+    {29},
+    {25},
+  };
 
-  math::GaussJordanEliminate(&d);
+  math::GaussJordanEliminate(&d, &dd);
 
-  assert(EQ(d[0][0], 1)); assert(EQ(d[0][1], 0));
-  assert(EQ(d[0][2], -1)); assert(EQ(d[0][3], -2));
+  assert(EQ(d[0][0], 1)); assert(EQ(d[0][1], 0)); assert(EQ(d[0][2], -1));
+  assert(EQ(d[1][0], 0)); assert(EQ(d[1][1], 1)); assert(EQ(d[1][2], 2));
 
-  assert(EQ(d[1][0], 0)); assert(EQ(d[1][1], 1));
-  assert(EQ(d[1][2], 2)); assert(EQ(d[1][3], 11));
+  assert(EQ(dd[0][0], -2));
+  assert(EQ(dd[1][0], 11));
 
-  // not augmented matrix
-  Matrix<double> e(1, 0);
-  // math::GaussJordanEliminate(&e);  // exception thrown
+  Matrix<double> e(3, 0);
+  math::GaussJordanEliminate(&e);  // exception thrown
 }
 
 int main(int argc, char* argv[]) {
