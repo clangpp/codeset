19bf43201413f513bafdf6d27f5b43ef755779d2
clangpp <clangpp@gmail.com>
2014-08-25 22:12:40 +0800

    Changes GaussJordanEliminate() to call GaussEliminate().

--- math/src/math/math.h
+++ math/src/math/math.h
@@ -50,72 +50,6 @@ struct TrivialIsZero {
   }
 };
 
-// absolute_less: Callable, `bool ret = absolute_less(v1, v2);` should be
-// is_zero: Callable, `bool ret = is_zero(v);` should be satified.
-//  e.g. is_zero(double v) -> std::abs(v) < 1e-6;
-template <typename T, typename AbsoluteLess = TrivialAbsLess<T>,
-          typename IsZero = TrivialIsZero<T>>
-void GaussJordanEliminate(
-    Matrix<T>* coefficient_matrix,
-    Matrix<T>* extra_matrix = nullptr,
-    AbsoluteLess absolute_less = AbsoluteLess(),
-    IsZero is_zero = IsZero()) {
-  if (extra_matrix) {
-    matrix::CheckAugmentable(*coefficient_matrix, *extra_matrix);
-  }
-
-  // Helpers
-  auto a_mat = coefficient_matrix;  // shorter name
-  auto b_mat = extra_matrix;  // shorter name
-  typedef typename Matrix<T>::size_type size_type;
-  typedef typename Matrix<T>::value_type value_type;
-  std::vector<std::future<void>> futures(a_mat->row_size());
-
-  size_type pivot_row = 0, pivot_column = 0;
-  for (; pivot_row < a_mat->row_size() &&
-      pivot_column < a_mat->column_size(); ++pivot_column) {
-    // Finds max (absolute) value of current column.
-    auto max_iter = std::max_element(
-        a_mat->column_begin(pivot_column) + pivot_row,
-        a_mat->column_end(pivot_column),
-        absolute_less);
-    size_type max_row = max_iter - a_mat->column_begin(pivot_column);
-    if (is_zero((*a_mat)[max_row][pivot_column])) {
-      continue;
-    }
-
-    // Switches `max_row` to `pivot_row`.
-    a_mat->elementary_row_switch(pivot_row, max_row);
-    if (b_mat) {
-      b_mat->elementary_row_switch(pivot_row, max_row);
-    }
-
-    // Concurrently eliminates `pivot_column`'s coefficients (in all rows
-    // except `pivot_row`).
-    ConcurrentProcess(
-        0, a_mat->row_size(),
-        [a_mat, b_mat, pivot_row, pivot_column](size_type row) {
-          if (row != pivot_row) {
-            value_type scaler = -(*a_mat)[row][pivot_column] /
-                (*a_mat)[pivot_row][pivot_column];
-            a_mat->elementary_row_add(row, pivot_row, scaler);
-            if (b_mat) {
-              b_mat->elementary_row_add(row, pivot_row, scaler);
-            }
-          }
-        }, &futures);
-
-    // Normalizes row `pivot`.
-    value_type scaler = 1 / (*a_mat)[pivot_row][pivot_column];
-    a_mat->elementary_row_multiply(pivot_row, scaler, is_zero);
-    if (b_mat) {
-      b_mat->elementary_row_multiply(pivot_row, scaler, is_zero);
-    }
-
-    ++pivot_row;
-  }
-}
-
 enum MatrixForm {
   kRowEchelonForm,
   kReducedRowEchelonForm,
@@ -229,6 +163,24 @@ typename Matrix<T>::size_type GaussEliminate(
 
   // Returns rank of coefficient_matrix.
   return pivot_row;
+}
+
+// absolute_less: Callable, `bool ret = absolute_less(v1, v2);` should be
+// is_zero: Callable, `bool ret = is_zero(v);` should be satified.
+//  e.g. is_zero(double v) -> std::abs(v) < 1e-6;
+template <typename T, typename AbsoluteLess = TrivialAbsLess<T>,
+          typename IsZero = TrivialIsZero<T>>
+void GaussJordanEliminate(
+    Matrix<T>* coefficient_matrix,
+    Matrix<T>* extra_matrix = nullptr,
+    AbsoluteLess absolute_less = AbsoluteLess(),
+    IsZero is_zero = IsZero()) {
+  GaussEliminate(
+      coefficient_matrix,
+      extra_matrix,
+      kReducedRowEchelonForm,
+      absolute_less,
+      is_zero);
 }
 
 }  // namespace math
