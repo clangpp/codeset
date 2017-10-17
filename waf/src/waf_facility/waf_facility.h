f24a1d11eb9b0e4719ce9133c3f4072a4dbbcd97
Yongtian Zhang <ytzhang@ytzhang.mtv.corp.google.com>
2017-10-17 03:13:28 -0700

    Adds clang++ makefile, fixes compile errors.

--- waf/src/waf_facility/waf_facility.h
+++ waf/src/waf_facility/waf_facility.h
@@ -20,193 +20,193 @@ namespace waf {
 // term to/from termid, termid existance predication
 // =============================================================================
 class TermSet {
-public:
-    typedef std::string term_type;
-    typedef waf::termid_type termid_type;
-    typedef waf::size_type size_type;
-private:
-    typedef ac::TrieMap<char, termid_type> term_set_type;
-    typedef std::vector<term_set_type::const_iterator> termid_set_type;
+ public:
+  typedef std::string term_type;
+  typedef waf::termid_type termid_type;
+  typedef waf::size_type size_type;
 
-public:  // insert term/termid mapping pair
-    bool insert(termid_type termid, const term_type& term);
-    bool insert(const term_type& term, termid_type termid) {
-        return insert(termid, term);
-    }
-    bool insert(const std::pair<termid_type, term_type>& mapping) {
-        return insert(mapping.first, mapping.second);
-    }
-    bool insert(const std::pair<term_type, termid_type>& mapping) {
-        return insert(mapping.first, mapping.second);
-    }
+ private:
+  typedef ac::TrieMap<char, termid_type> term_set_type;
+  typedef std::vector<term_set_type::const_iterator> termid_set_type;
 
-public:  // remove term/termid mapping pair
-    bool erase(termid_type termid);
-    bool erase(const term_type& term);
+ public:  // insert term/termid mapping pair
+  bool insert(termid_type termid, const term_type& term);
+  bool insert(const term_type& term, termid_type termid) {
+    return insert(termid, term);
+  }
+  bool insert(const std::pair<termid_type, term_type>& mapping) {
+    return insert(mapping.first, mapping.second);
+  }
+  bool insert(const std::pair<term_type, termid_type>& mapping) {
+    return insert(mapping.first, mapping.second);
+  }
 
-public:  // query term/termid existance
-    bool search(const term_type& term) const;
-    bool search(termid_type termid) const;
-    size_type count(const term_type& term) const { return search(term); }
-    size_type count(termid_type termid) const { return search(termid); }
+ public:  // remove term/termid mapping pair
+  bool erase(termid_type termid);
+  bool erase(const term_type& term);
 
-public:  // query term/termid mapping, never change set
-    termid_type operator [] (const term_type& term) const;
-    term_type operator [] (termid_type termid) const;
+ public:  // query term/termid existance
+  bool search(const term_type& term) const;
+  bool search(termid_type termid) const;
+  size_type count(const term_type& term) const { return search(term); }
+  size_type count(termid_type termid) const { return search(termid); }
 
-public:  // observers
-    size_type size() const { return term_set_.size(); }
-    termid_type max_termid() const { return termid_set_.size()-1; }
-    void clear() {
-        term_set_.clear();
-        termid_set_.clear();
-    }
+ public:  // query term/termid mapping, never change set
+  termid_type operator[](const term_type& term) const;
+  term_type operator[](termid_type termid) const;
 
-private:
-    void remove(termid_type termid, const term_type& term);
+ public:  // observers
+  size_type size() const { return term_set_.size(); }
+  termid_type max_termid() const { return termid_set_.size() - 1; }
+  void clear() {
+    term_set_.clear();
+    termid_set_.clear();
+  }
 
-private:  // serialization
-    friend std::istream& operator >> (std::istream& is, TermSet& termset);
-    friend std::ostream& operator << (std::ostream& os, const TermSet& termset);
+ private:
+  void remove(termid_type termid, const term_type& term);
 
+ private:  // serialization
+  friend std::istream& operator>>(std::istream& is, TermSet& termset);
+  friend std::ostream& operator<<(std::ostream& os, const TermSet& termset);
 
-private:
-    term_set_type term_set_;
-    termid_set_type termid_set_;
+ private:
+  term_set_type term_set_;
+  termid_set_type termid_set_;
 };
 
 // unlimited term frequency vector
 // =============================================================================
 class FreqVector {
-public:
-    typedef waf::termid_type termid_type;
-    typedef waf::size_type size_type;
-    class iterator;
-    class const_iterator;
+ public:
+  typedef waf::termid_type termid_type;
+  typedef waf::size_type size_type;
+  class iterator;
+  class const_iterator;
 
-private:
-    template <typename FreqVecPointer>
-    class basic_iterator:
-        public std::iterator<std::random_access_iterator_tag, size_type> {
-    public:  // constructors
-        friend class iterator;
-        friend class const_iterator;
-        basic_iterator(FreqVecPointer pfreq_vec, size_type pos):
-            pfreq_vec_(pfreq_vec), pos_(pos) {}
-        basic_iterator(): pfreq_vec_(NULL), pos_(-1) {}
-    public:  // member functions  // TBD: more members to be written
-        basic_iterator operator + (difference_type n) const {
-            return basic_iterator(pfreq_vec_, pos_+n);
-        }
-    protected:
-        FreqVecPointer pfreq_vec_;
-        size_type pos_;
-    };
+ private:
+  template <typename FreqVecPointer>
+  class basic_iterator
+      : public std::iterator<std::random_access_iterator_tag, size_type> {
+   public:  // constructors
+    friend class FreqVector::iterator;
+    friend class FreqVector::const_iterator;
+    basic_iterator(FreqVecPointer pfreq_vec, size_type pos)
+        : pfreq_vec_(pfreq_vec), pos_(pos) {}
+    basic_iterator() : pfreq_vec_(NULL), pos_(-1) {}
 
-public:
-    class const_iterator: public basic_iterator<const FreqVector*> {
-    public:
-        typedef basic_iterator<const FreqVector*> base;
-        const_iterator(const FreqVector* pfreq_vec, size_type pos):
-            base(pfreq_vec, pos) {}
-        const_iterator(): base() {}
-        const_iterator(const base& iter): base(iter) {}
-    public:
-        size_type operator * () const { return (*pfreq_vec_)[pos_]; }
-        const_iterator operator + (difference_type n) const {
-            return base::operator + (n);
-        }
-        size_type operator [] (difference_type n) const {
-            return * const_iterator(*this + n);
-        }
-    };
-
-    class iterator: public basic_iterator<FreqVector*> {
-    public:
-        typedef basic_iterator<FreqVector*> base;
-        friend class const_iterator;
-    public:
-        iterator(FreqVector* pfreq_vec, size_type pos): base(pfreq_vec, pos) {}
-        iterator(): base() {}
-        iterator(const base& iter): base(iter) {}
-    public:
-        size_type& operator * () { return (*base::pfreq_vec_)[base::pos_]; }
-        iterator operator + (difference_type n) const {
-            return base::operator + (n);
-        }
-        size_type& operator [] (difference_type n) {
-            return * iterator(*this + n);
-        }
-        operator const_iterator() const {
-            return const_iterator(base::pfreq_vec_, base::pos_);
-        }
-    };
-
-public:  // iterator observers
-    iterator begin() { return iterator(this, 0); }
-    iterator end() { return iterator(this, term_freqs_.size()); }
-    const_iterator begin() const { return const_iterator(this, 0); }
-    const_iterator end() const {
-        return const_iterator(this, term_freqs_.size());
+   public:  // member functions  // TBD: more members to be written
+    basic_iterator operator+(difference_type n) const {
+      return basic_iterator(pfreq_vec_, pos_ + n);
     }
 
-public:  // term frequency observers
-    size_type& operator [] (termid_type termid);
-    size_type operator [] (termid_type termid) const;
+   protected:
+    FreqVecPointer pfreq_vec_;
+    size_type pos_;
+  };
 
-public:  // member functions  // TBD: more functions to be written
-    void clear() { term_freqs_.clear(); }
-    size_type size() const { return term_freqs_.size(); }
+ public:
+  class const_iterator : public basic_iterator<const FreqVector*> {
+   public:
+    typedef basic_iterator<const FreqVector*> base;
+    const_iterator(const FreqVector* pfreq_vec, size_type pos)
+        : base(pfreq_vec, pos) {}
+    const_iterator() : base() {}
+    const_iterator(const base& iter) : base(iter) {}
 
-public:  // serialization
-    friend std::istream& operator >> (std::istream& is, FreqVector& freqvec);
-    friend std::ostream& operator << (
-            std::ostream& os, const FreqVector& freqvec);
+   public:
+    size_type operator*() const { return (*pfreq_vec_)[pos_]; }
+    const_iterator operator+(difference_type n) const {
+      return base::operator+(n);
+    }
+    size_type operator[](difference_type n) const {
+      return *const_iterator(*this + n);
+    }
+  };
 
-private:
-    std::vector<size_type> term_freqs_;
+  class iterator : public basic_iterator<FreqVector*> {
+   public:
+    typedef basic_iterator<FreqVector*> base;
+    friend class const_iterator;
+
+   public:
+    iterator(FreqVector* pfreq_vec, size_type pos) : base(pfreq_vec, pos) {}
+    iterator() : base() {}
+    iterator(const base& iter) : base(iter) {}
+
+   public:
+    size_type& operator*() { return (*base::pfreq_vec_)[base::pos_]; }
+    iterator operator+(difference_type n) const { return base::operator+(n); }
+    size_type& operator[](difference_type n) { return *iterator(*this + n); }
+    operator const_iterator() const {
+      return const_iterator(base::pfreq_vec_, base::pos_);
+    }
+  };
+
+ public:  // iterator observers
+  iterator begin() { return iterator(this, 0); }
+  iterator end() { return iterator(this, term_freqs_.size()); }
+  const_iterator begin() const { return const_iterator(this, 0); }
+  const_iterator end() const {
+    return const_iterator(this, term_freqs_.size());
+  }
+
+ public:  // term frequency observers
+  size_type& operator[](termid_type termid);
+  size_type operator[](termid_type termid) const;
+
+ public:  // member functions  // TBD: more functions to be written
+  void clear() { term_freqs_.clear(); }
+  size_type size() const { return term_freqs_.size(); }
+
+ public:  // serialization
+  friend std::istream& operator>>(std::istream& is, FreqVector& freqvec);
+  friend std::ostream& operator<<(std::ostream& os, const FreqVector& freqvec);
+
+ private:
+  std::vector<size_type> term_freqs_;
 };
 
 // Cell compare by value
 // =============================================================================
 template <typename T>
-bool cell_value_greater(
-        const serialization::sparsematrix::Cell<T>& lhs,
-        const serialization::sparsematrix::Cell<T>& rhs);
+bool cell_value_greater(const serialization::sparsematrix::Cell<T>& lhs,
+                        const serialization::sparsematrix::Cell<T>& rhs);
 
 // predicate facility
 // =============================================================================
 // class Care, to predicate whether a termid is cared or not
 class Care {
-public:
-    Care(const TermSet& termset): pterm_set_(&termset) {}
-    Care(): pterm_set_(NULL) {}  // always return true
-    bool operator () (termid_type termid) const {
-        return pterm_set_ ? pterm_set_->search(termid) : true;
-    }
-private:
-    const TermSet* pterm_set_;
+ public:
+  Care(const TermSet& termset) : pterm_set_(&termset) {}
+  Care() : pterm_set_(NULL) {}  // always return true
+  bool operator()(termid_type termid) const {
+    return pterm_set_ ? pterm_set_->search(termid) : true;
+  }
+
+ private:
+  const TermSet* pterm_set_;
 };
 
 // factory method: create a Predicate function boject
 inline Care care_in(const TermSet& termset) { return Care(termset); }
 inline Care care_all() { return Care(); }
 
-
 // class FreqVecObserver, to query frequency of a termid
 class FreqVecObserver {
-public:
-    FreqVecObserver(const FreqVector& freqvec): pfreq_vec_(&freqvec) {}
-    size_type operator () (termid_type termid) const {
-        return (*pfreq_vec_)[termid];
-    }
-private:
-    const FreqVector* pfreq_vec_;
+ public:
+  FreqVecObserver(const FreqVector& freqvec) : pfreq_vec_(&freqvec) {}
+  size_type operator()(termid_type termid) const {
+    return (*pfreq_vec_)[termid];
+  }
+
+ private:
+  const FreqVector* pfreq_vec_;
 };
 
 // factory method: create a UnaryFunction function object
 inline FreqVecObserver freq_dict(const FreqVector& freqvec) {
-    return FreqVecObserver(freqvec);
+  return FreqVecObserver(freqvec);
 }
 
 }  // namespace waf
